1ddf6a1b515135c4745f45dc95812db36897620d
Ishan Vyas <ishan@deepsource.io>
2021-09-21 12:47:39 +0530

    fix: fatal error
    Signed-off-by: Ishan Vyas <ishan@deepsource.io>

--- src/AbstractMethodInNonAbstractClass.php
+++ src/AbstractMethodInNonAbstractClass.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App;
 
-class AbstractMethodInNonAbstractClass
-{
-    // invalid: contains 1 abstract method and must therefore be declared abstract or implement
-    abstract public function run(): void;
-}
+// class AbstractMethodInNonAbstractClass
+// {
+// 	// invalid: contains 1 abstract method and must therefore be declared abstract or implement
+//     abstract public function run(): void;
+// }
